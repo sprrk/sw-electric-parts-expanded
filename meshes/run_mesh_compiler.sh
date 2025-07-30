d44447c0b1f4b870f2ea0abe87e57bc55a1ae01e
sprk <sprrk@pm.me>
2025-07-30 15:31:35 +0200

    Remove mesh compiler shell script

--- meshes/run_mesh_compiler.sh
+++
@@ -1,5 +0,0 @@
-filename=$1
-filepath=/home/deck/.steam/root/steamapps/compatdata/573090/pfx/drive_c/users/steamuser/AppData/Roaming/Stormworks/data/mods/electric_parts_expanded/meshes/
-compiler_path=/home/deck/.steam/steam/steamapps/common/Stormworks/sdk/mesh_compiler.com
-
-STEAM_COMPAT_CLIENT_INSTALL_PATH=/home/deck/.steam/root/steamapps STEAM_COMPAT_DATA_PATH=/home/deck/.steam/root/steamapps/compatdata/573090 /home/deck/.local/share/Steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run ${compiler_path} ${filepath}/${filename} -o ${filepath}/ -s
