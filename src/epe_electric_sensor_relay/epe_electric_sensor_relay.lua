0ee7fc6f7aab084814494fdbc36c466e7df4793f
sprk <sprrk@pm.me>
2025-11-02 13:28:23 +0100

    Fix lua lint errors

--- src/epe_electric_sensor_relay/epe_electric_sensor_relay.lua
+++ src/epe_electric_sensor_relay/epe_electric_sensor_relay.lua
@@ -1,12 +1,12 @@
-function onTick(tick_time)
-    local connected, connected_ok = component.slotElectricIsConnected(0)
-    if connected and connected_ok then
-        local charge, get_charge_ok = component.slotElectricGetChargeFactor(0)
+function onTick(_)
+	local connected, connected_ok = component.slotElectricIsConnected(0)
+	if connected and connected_ok then
+		local charge, get_charge_ok = component.slotElectricGetChargeFactor(0)
 
-        if get_charge_ok and charge > 0 then
-            component.setOutputLogicSlotBool(0, true)
-            return
-        end
-    end
-    component.setOutputLogicSlotBool(0, false)
+		if get_charge_ok and charge > 0 then
+			component.setOutputLogicSlotBool(0, true)
+			return
+		end
+	end
+	component.setOutputLogicSlotBool(0, false)
 end
--- src/epe_power_regulator/epe_power_regulator.lua
+++ src/epe_power_regulator/epe_power_regulator.lua
@@ -1,21 +1,31 @@
-function onTick(tick_time)
-    local charge_rate, _ = component.getInputLogicSlotFloat(0)
-    local charge_target, _ = component.getInputLogicSlotFloat(1)
-    if charge_rate < 0 then charge_rate = 0 end
-    if charge_rate > 1 then charge_rate = 1 end
-    if charge_target < 0 then charge_target = 0 end
-    if charge_target > 1 then charge_target = 1 end
+function onTick(_)
+	local charge_rate, _ = component.getInputLogicSlotFloat(0)
+	local charge_target, _ = component.getInputLogicSlotFloat(1)
+	if charge_rate < 0 then
+		charge_rate = 0
+	end
+	if charge_rate > 1 then
+		charge_rate = 1
+	end
+	if charge_target < 0 then
+		charge_target = 0
+	end
+	if charge_target > 1 then
+		charge_target = 1
+	end
 
-    local a_connected, a_ok = component.slotElectricIsConnected(0)
-    local b_connected, b_ok = component.slotElectricIsConnected(1)
-    if a_connected and b_connected and a_ok and b_ok then
-        local charge_a, a_ok = component.slotElectricGetChargeFactor(0)
-        local charge_b, b_ok = component.slotElectricGetChargeFactor(1)
+	local a_connected, a_ok = component.slotElectricIsConnected(0)
+	local b_connected, b_ok = component.slotElectricIsConnected(1)
+	if a_connected and b_connected and a_ok and b_ok then
+		local charge_a, c_ok = component.slotElectricGetChargeFactor(0)
+		local charge_b, d_ok = component.slotElectricGetChargeFactor(1)
 
-        if a_ok and b_ok and charge_a > 0 and charge_b < charge_target then
-            charge_rate = charge_rate * (1 - (charge_target*(charge_b^64)))
-            local charge_factor, ok = component.slotElectricRemoveCharge(0, charge_rate)
-            if ok then component.slotElectricAddCharge(1, charge_rate*charge_factor*0.95) end
-        end
-    end
+		if c_ok and d_ok and charge_a > 0 and charge_b < charge_target then
+			charge_rate = charge_rate * (1 - (charge_target * (charge_b ^ 64)))
+			local charge_factor, ok = component.slotElectricRemoveCharge(0, charge_rate)
+			if ok then
+				component.slotElectricAddCharge(1, charge_rate * charge_factor * 0.95)
+			end
+		end
+	end
 end
